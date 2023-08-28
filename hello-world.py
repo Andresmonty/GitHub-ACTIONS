d8685328c01869fdaed691484f2d4de6093165f4
ProM2Monty <prom2monty@Monty-Pro.local>
2023-08-28 16:19:54 -0500

    modify hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,6 @@
 import os
 def main():
     nombre = os.getenv("USERNAME")
-    print(f"¡Hola, {nombre} desde los GitHubs!")
+    print(f"¡Hello, Mr {nombre} desde los GitHubs!")
 if __name__ == "__main__":
     main()
